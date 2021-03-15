dd608d565133742edfd4afb99bb337a9dad1b523
Ryota Ushio <gt0410ushi@g.ecc.u-tokyo.ac.jp>
2021-03-16 00:20:25 +0900

    indexing complete

--- metadata.hpp
+++ metadata.hpp
@@ -1,7 +1,6 @@
 #pragma once
 #include <typeinfo> // for dtype
 #include <vector>
-#include <cmath> // abs
 #include <stdexcept> // invalid_argument
 #include "utils.hpp"
 #include "slice.hpp"
@@ -29,37 +28,42 @@ namespace numpy {
     const std::type_info& dtype;
 
     array_metadata(shape_type shape_, stride_type stride_, offset_type offset_=0)
-      : shape(shape_), offset(offset_), stride(stride_), dtype(typeid(T)) {
-      size = utils::product(shape);
-      ndim = shape.size();
+      : shape(shape_), stride(stride_), offset(offset_), dtype(typeid(T)) {
+      adjust_to_shape();
       if (stride.size() != ndim)
 	throw std::invalid_argument("Stride and shape must have the same length.");
     }
 
     array_metadata(shape_type shape_, offset_type offset_=0)
-      : array_metadata(shape_, stride_type(shape_.size(), 1), offset_) {}
+      : shape(shape_), offset(offset_), dtype(typeid(T)) {
+      adjust_to_shape();
+      stride = stride_type(shape.size());
+      stride[ndim-1] = 1;
+      for(int i=ndim-2; i>=0; i--){
+	stride[i] = stride[i+1] * shape[i+1];
+      }
+    }
     
     array_metadata(const array_metadata& src)
       : shape(src.shape), ndim(src.ndim), size(src.size), offset(src.offset), stride(src.stride), dtype(src.dtype) {}
 
-    void reshape(const shape_type& newshape) {
-      size_type size_ = utils::product(newshape);
-      if (size_ != size) 
-	throw std::invalid_argument("Imcompatible size");
+    void set_shape(const shape_type& newshape) {
       shape = newshape;
-      size = size_;
+      adjust_to_shape();
+    }
+
+    void adjust_to_shape() {
+      size = utils::product(shape);
       ndim = shape.size();
     }
 
     void _indexer_inplace_impl(dim_type axis) {}
-    // template <class... Tail> void _indexer_inplace_impl(dim_type axis, int head, Tail... tail);
-    // template <class... Tail> void _indexer_inplace_impl(dim_type axis, slice head, Tail... tail);
     
     template <class... Tail>
     void _indexer_inplace_impl(dim_type axis, slice head, Tail... tail) {
-      offset += head.start(shape[axis]) * stride[axis];
-      shape[axis] = std::abs(head.size(shape[axis]));
-      stride[axis] *= head.step();
+      offset += head.abs_start(shape[axis]) * stride[axis];
+      shape[axis] = head.size(shape[axis]);
+      stride[axis] *= head.step;
       _indexer_inplace_impl(axis + 1, tail...);
     }
 
@@ -68,12 +72,16 @@ namespace numpy {
       offset += head * stride[axis];
       shape.erase(shape.begin() + axis);
       stride.erase(stride.begin() + axis);
-      _indexer_inplace_impl(axis + 1, tail...);
+      _indexer_inplace_impl(axis, tail...);
     }
 
     template <class... Index>
     void indexer_inplace(Index... indices) {
+      if (sizeof...(indices) > ndim) {
+	throw std::invalid_argument("IndexError: too many indices for array: array is " + std::to_string(ndim) + "-dimensional, but " + std::to_string(sizeof...(indices)) + " were indexed");
+      }      
       _indexer_inplace_impl(0, indices...);
+      adjust_to_shape();
     }
 
     template <class... Index>
@@ -81,7 +89,16 @@ namespace numpy {
       array_metadata<T> newmeta(*this);
       newmeta.indexer_inplace(indices...);
       return newmeta;
-    }    
+    }
+
+    // for debug
+    void print() {
+      std::cout << "shape: "; utils::print_vector(shape);
+      std::cout << "size: " << size << std::endl;
+      std::cout << "ndim: " << ndim << std::endl;
+      std::cout << "offset: " << offset << std::endl;
+      std::cout << "stride: "; utils::print_vector(stride);
+    }
   };
   
 }
