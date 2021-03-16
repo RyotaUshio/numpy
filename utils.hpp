1a553fd1c31555d17d990bbcf17195a3d046c72f
Ryota Ushio <gt0410ushi@g.ecc.u-tokyo.ac.jp>
2021-03-16 20:31:24 +0900

    a

--- utils.hpp
+++ utils.hpp
@@ -56,7 +56,7 @@ namespace numpy {
     template <typename T>
     void test(ndarray<T>& a) {
       std::cout << "dtype : " << a.dtype.name() << std::endl;
-      std::cout << "__repr__() :" << std::endl;;
+      std::cout << "__repr__() : " << std::endl;;
       std::cout << a.__repr__() << std::endl;
       line();
       std::cout << "range-based for loop" << std::endl;
