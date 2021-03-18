0d0f399bdf4f2b769cadb9abbfd76e021b1a6cf7
Ryota Ushio <gt0410ushi@g.ecc.u-tokyo.ac.jp>
2021-03-18 12:11:56 +0900

    implemented copy assignment operator. see test/test_array_copy.cpp for examples

--- test/test_array_copy.cpp
+++ test/test_array_copy.cpp
@@ -1,3 +1,31 @@
+// >>> auto a = np::ndarray<np::float64>({0, 1, 2, 3}, {2, 2});
+
+// >>> auto b = a;
+// array_view::(copy constructor) is called
+
+// >>> print(a);
+// array([[0.000000,	1.000000],
+// 	[2.000000,	3.000000]])
+
+// >>> print(a.T());
+// array([[0.000000,	2.000000],
+// 	[1.000000,	3.000000]])
+
+// >>> b = a;
+// array_view::(copy constructor) is called
+// ndarray::(copy assignment operator) is called
+// ndarray::swap is called
+// array_view::swap is called
+// array_view::swap is called
+
+// >>> print(b);
+// array([[0.000000,	1.000000],
+// 	[2.000000,	3.000000]])
+
+// >>> print(a);
+// array([[0.000000,	1.000000],
+// 	[2.000000,	3.000000]])
+
 #include <numpy/python.hpp>
 #include <numpy/numpy.hpp>
 using namespace python;
