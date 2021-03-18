1368ee7620714c75e08b09a264384d9dd6edab6f
Ryota Ushio <gt0410ushi@g.ecc.u-tokyo.ac.jp>
2021-03-18 12:10:40 +0900

    implemented copy assignment operator. see test/test_array_copy.cpp for examples

--- test/test_array_copy.cpp
+++ test/test_array_copy.cpp
@@ -7,8 +7,8 @@ int main() {
   print(">>> auto a = np::ndarray<np::float64>({0, 1, 2, 3}, {2, 2});");
   auto a = np::ndarray<np::float64>({0, 1, 2, 3}, {2, 2});
 
-  print("\n>>> np::ndarray<np::float64> b = a;");
-  np::ndarray<np::float64> b = a;
+  print("\n>>> auto b = a;");
+  auto b = a;
 
   print("\n>>> print(a);");
   print(a);
